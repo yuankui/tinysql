f48b9dccf9537783babe001c031d542c33ad49e5
yuankui <yuankui@meituan.com>
2021-10-13 17:30:25 +0800

    clear unsued

--- tinysql-fe/src/app/views/right/editor/SqlEditorView.tsx
+++ tinysql-fe/src/app/views/right/editor/SqlEditorView.tsx
@@ -1,4 +1,3 @@
-import { Editor, EditorSelectionChange } from 'codemirror'
 import 'codemirror/mode/sql/sql'
 import { FunctionComponent } from 'react'
 import { UnControlled } from 'react-codemirror2'
@@ -13,26 +12,6 @@ const SqlEditorView: FunctionComponent<SqlEditorView2Props> = () => {
                 mode: 'sql',
                 theme: 'material',
                 lineNumbers: true,
-                gutters: ['run'],
-            }}
-            onSelection={(editor: Editor, data: EditorSelectionChange) => {
-                const lines = data.ranges
-                    .filter(
-                        (r) =>
-                            r.anchor.line !== r.head.line ||
-                            r.anchor.ch !== r.head.ch
-                    )
-                    .map((r) => Math.min(r.anchor.line, r.head.line))
-
-                if (lines.length === 0) {
-                    return;
-                }
-                const minLine = lines.reduce((l, r) => Math.min(l, r))
-
-                const span = document.createElement('span')
-                span.className = 'bg-blue-400 flex w-4 h-4'
-                editor.clearGutter('run');
-                editor.setGutterMarker(minLine, 'run', span)
             }}
             onChange={(editor, data, value) => {}}
         />
