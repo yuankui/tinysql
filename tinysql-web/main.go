377334df561aa955003235aee3b3c49b49f8528a
yuankui <yuankui@meituan.com>
2021-10-14 10:31:22 +0800

    up

--- tinysql-web/main.go
+++ tinysql-web/main.go
@@ -1,9 +1,11 @@
 package main
 
 import "github.com/go-martini/martini"
+import "github.com/martini-contrib/render"
 
 func main() {
 	m := martini.Classic()
+	m.Use(render.Renderer())
 	m.Get("/", func() string {
 		return "Hello world!"
 	})
