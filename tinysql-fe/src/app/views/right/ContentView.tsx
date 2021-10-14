1853642a08431889541911ce149c2574e878e44b
yuankui <yuankui@meituan.com>
2021-10-14 15:30:04 +0800

    add page

--- tinysql-fe/src/app/views/right/ContentView.tsx
+++ tinysql-fe/src/app/views/right/ContentView.tsx
@@ -7,18 +7,28 @@ import SqlTableOutputView from './output/SqlTableOutputView'
 
 interface ContentViewProps {}
 
-const ContentView: FunctionComponent<ContentViewProps> = () => {
-    const [result, setResult] = useState<TableResult>();
-    const api = useApi();
+const PAGE_SIZE = 50
 
+const ContentView: FunctionComponent<ContentViewProps> = () => {
+    const [result, setResult] = useState<TableResult>()
+    const api = useApi()
+    const [sql, setSql] = useState('')
+    const [page, setPage] = useState(0)
     return (
         <Tabs defaultActiveKey="1" type="card" size={'middle'}>
             <Tabs.TabPane tab="Card Tab 1" key="1">
-                <SqlEditorView onExec={async sql => {
-                    const res = await api.getQueryResult(1, "", "");
-                    setResult(res);
-                }} />
-                <SqlTableOutputView data={result}/>
+                <SqlEditorView
+                    onExec={async (sql) => {
+                        const res = await api.getQueryResult(1, '', '')
+                        setResult(res)
+                    }}
+                />
+                <SqlTableOutputView
+                    data={result}
+                    page={page}
+                    onPrev={() => setPage((prev) => prev - 1)}
+                    onNext={() => setPage((prev) => prev + 1)}
+                />
             </Tabs.TabPane>
             <Tabs.TabPane tab="Card Tab 2" key="2">
                 Content of card tab 2
--- tinysql-fe/src/app/views/right/output/SqlTableOutputView.tsx
+++ tinysql-fe/src/app/views/right/output/SqlTableOutputView.tsx
@@ -1,21 +1,33 @@
+import { LeftOutlined, RightOutlined } from '@ant-design/icons'
+import { Button } from 'antd'
 import { FunctionComponent } from 'react'
 import { TableResult } from '../../../api'
 
 interface SqlTableOutputViewProps {
     data?: TableResult
+    onNext(): any
+    onPrev(): any
+    page: number
 }
 
 const SqlTableOutputView: FunctionComponent<SqlTableOutputViewProps> = ({
     data,
+    page,
+    onNext,
+    onPrev,
 }) => {
     if (data === undefined) {
         return <div>EMPTY</div>
     }
+
+    const disablePrev = page <= 0
+    const disableNext = data.data.length === 0
+
     return (
         <div className="flex-1">
             <table>
-                <thead className='bg-gray-200'>
-                    <tr className=''>
+                <thead className="bg-gray-200">
+                    <tr className="">
                         {(data || {}).fields.map((d, i) => {
                             return <td key={i}>{d}</td>
                         })}
@@ -33,6 +45,31 @@ const SqlTableOutputView: FunctionComponent<SqlTableOutputViewProps> = ({
                     })}
                 </tbody>
             </table>
+            <div className="flex flex-row justify-end">
+                <div className="flex flex-row p-4 gap-2">
+                    <Button
+                        disabled={disablePrev}
+                        size="small"
+                        onClick={(e) => {
+                            onPrev()
+                        }}
+                    >
+                        <LeftOutlined />
+                        上一页
+                    </Button>
+                    <Button size="small">{page}</Button>
+                    <Button
+                        disabled={disableNext}
+                        size="small"
+                        onClick={(e) => {
+                            onNext()
+                        }}
+                    >
+                        下一页
+                        <RightOutlined />
+                    </Button>
+                </div>
+            </div>
         </div>
     )
 }
