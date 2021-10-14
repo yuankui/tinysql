bca86de3fa777cf97450a28e28002a2cc78dc3ea
yuankui <yuankui@meituan.com>
2021-10-14 10:37:23 +0800

    up

--- tinysql-web/main.go
+++ tinysql-web/main.go
@@ -1,13 +1,29 @@
 package main
 
-import "github.com/go-martini/martini"
-import "github.com/martini-contrib/render"
+import (
+	"os"
+
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
+)
+
+type Person struct {
+	Name string `json: "name"`
+	Age  int    `json:"age"`
+}
 
 func main() {
+	// set port
+	os.Setenv("PORT", "3001")
+
+	// init server
 	m := martini.Classic()
 	m.Use(render.Renderer())
-	m.Get("/", func() string {
-		return "Hello world!"
+	m.Get("/", func(r render.Render) {
+		r.JSON(200, Person{
+			Name: "yuankui",
+			Age:  11,
+		})
 	})
 	m.Run()
 }
